58720c4dbb2d28edb485ced3c3a195ce1c0290e1
Pushpa 2 Download <sahoriajnajimjoy@gmail.com>
2025-01-21 15:06:18 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+// List of all pages and posts on your site
+const urls = [
+  'https://cineverse.linkpc.net/',
+  'https://cineverse.linkpc.net/about.html',
+  'https://cineverse.linkpc.net/contact.html',
+  'https://cineverse.linkpc.net/post1',
+  'https://cineverse.linkpc.net/post2',
+  // Add more URLs for posts and pages
+];
+
+// Generate the XML sitemap content
+const generateSitemap = (urls) => {
+  const xmlHeader = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+  const xmlFooter = `</urlset>`;
+  const xmlBody = urls.map(url => {
+    return `
+    <url>
+      <loc>${url}</loc>
+      <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>`;
+  }).join('\n');
+
+  return xmlHeader + xmlBody + xmlFooter;
+};
+
+// Save the sitemap as an XML file
+const sitemap = generateSitemap(urls);
+fs.writeFileSync(path.join(__dirname, 'sitemap.xml'), sitemap);
+console.log('Sitemap generated successfully!');
